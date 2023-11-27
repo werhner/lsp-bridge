871aff65af5abd138f74b380f08fd9b4c62c1848
werhner <gao9426@hotmail.com>
2023-11-27 21:13:17 +0800

    gtags ref popup

--- core/gtags.py
+++ core/gtags.py
@@ -75,16 +75,66 @@ class Gtags(tags):
         cmd = self.global_get_cmd(symbol, "reference", True, None)
         lines = self.run_cmd_in_path(cmd, filepath)
 
-        def make_xref(line):
-            gtags_result = line.split(":", 2)
-            return {"ext-abspath": os.path.join(dir_name, gtags_result[0]),
-                    "line": int(gtags_result[1]),
-                    "column": 0,
-                    "summary": gtags_result[2]}
+        # def make_xref(line):
+        #     gtags_result = line.split(":", 2)
+        #     return {"ext-abspath": os.path.join(dir_name, gtags_result[0]),
+        #             "line": int(gtags_result[1]),
+        #             "column": 0,
+        #             "summary": gtags_result[2]}
 
-        candidates = map(make_xref, lines)
-        #print(list(candidates))
-        eval_in_emacs("lsp-bridge-xref-callback", list(candidates))
+        # candidates = map(make_xref, lines)
+        # #print(list(candidates))
+        # eval_in_emacs("lsp-bridge-xref-callback", list(candidates))
+
+        REFERENCE_PATH = '\033[95m'
+        REFERENCE_TEXT = '\033[94m'
+        REFERENCE_ENDC = '\033[0m'
+
+        # def make_lsp_popup(line):
+        #     gtags_result = line.split(":", 2)
+        #     path = os.path.join(dir_name, gtags_result[0])
+        #     line = int(gtags_result[1])
+        #     line_content = gtags_result[2]
+        #     start_column = line_content.find(symbol)
+        #     end_column = start_column + len(symbol)
+            
+        #     content = "".join(["\n", REFERENCE_PATH, path, REFERENCE_ENDC, "\n"])
+
+        #     content += "{}:{}:{}".format(
+        #         line + 1,
+        #         start_column,
+        #         "".join([line_content[:start_column], REFERENCE_TEXT, line_content[start_column:end_column], REFERENCE_ENDC, line_content[end_column:]])
+        #     )
+
+        #     return content
+
+        # candidates = map(make_lsp_popup, lines)
+        # list_candidates = list(candidates)
+        # print(list_candidates)
+        content = ""
+        pre_filepath = ""
+        for line in lines:
+            gtags_result = line.split(":", 2)
+            path = os.path.join(dir_name, gtags_result[0])
+            line = int(gtags_result[1])
+            line_content = gtags_result[2]
+            start_column = line_content.find(symbol)
+            end_column = start_column + len(symbol)
+            
+            if pre_filepath == path:
+                pass
+            else:
+                content += "".join(["\n", REFERENCE_PATH, path, REFERENCE_ENDC, "\n"])
+
+            pre_filepath = path
+
+            content += "{}:{}:{}\n".format(
+                line,
+                start_column,
+                "".join([line_content[:start_column], REFERENCE_TEXT, line_content[start_column:end_column], REFERENCE_ENDC, line_content[end_column:]])
+            )
+            
+        eval_in_emacs("lsp-bridge-references--popup", content, len(lines), 0)
 
     def global_get_cmd(self, name, mode, case_fold, start_file):
         cmd = []
--- test-global.py
+++ test-global.py
@@ -10,6 +10,9 @@ gtags.find_definition("CMCOST", "/home/werhner/code/emacs/doc/emacs")
 (lsp-bridge-gtags-find-ref "CMCOST" "/home/werhner/code/emacs/doc/emacs")
 
 (lsp-bridge-ctags-find-def "cmcheckmagic" "/home/werhner/code/emacs/doc/emacs")
+(lsp-bridge-ctags-find-def "CMCOST" "/home/werhner/code/emacs/doc/emacs")
+
+(lsp-bridge-gtags-find-ref "buffer_p" "/home/werhner/code/emacs/doc/emacs")
 
 (propertize (ansi-color-apply "\033[95m({})\033[0m{}"))
 (xterm-color-filter-strip "\033[95m({})\033[0m{}")
